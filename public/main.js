27f31e078f4eb8c02181f1216b7669448e3efaa3
RjayBrown <rjbrown.dev@outlook.com>
2024-09-25 21:48:20 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,196 @@
+// Page Theme
+const pageTheme = document.querySelector('html')
+const themeToggle = document.querySelector('.theme')
+const header = document.querySelector('header')
+const cards = document.querySelectorAll('.greeting')
+const likeText = document.querySelectorAll('.like-text')
+const likeBtn = document.querySelectorAll('.likeBtn')
+
+// Get or set theme on page load
+if (!localStorage.getItem('theme')) {
+    pageTheme.className = 'dark'
+    header.className = 'header-dark'
+    themeToggle.src = 'sun.png'
+    likeText.forEach(word => {
+        word.classList.add('like-dark')
+    })
+    likeBtn.forEach(btn => {
+        btn.classList.add('likeBtn-dark')
+        btn.src = 'like.png'
+        btn.addEventListener('mouseover', () => {
+            btn.src = 'like3.png'
+        })
+
+        btn.addEventListener('mouseout', () => {
+            btn.src = 'like.png'
+        })
+    })
+} else {
+    pageTheme.className = localStorage.getItem('theme')
+    header.className = localStorage.getItem('header')
+    themeToggle.src = localStorage.getItem('theme-img')
+    likeText.forEach(word => {
+        word.classList.add(localStorage.getItem('like-text'))
+    })
+    cards.forEach(card => {
+        card.classList.add(localStorage.getItem('greeting-card'))
+    })
+    likeBtn.forEach(btn => {
+        btn.classList.add(localStorage.getItem('btn'))
+        btn.src = localStorage.getItem('btn-img')
+        btn.addEventListener('mouseover', () => {
+            btn.src = 'like3.png'
+        })
+
+        btn.addEventListener('mouseout', () => {
+            btn.src = localStorage.getItem('btn-img')
+        })
+    })
+}
+
+// Toggle Theme
+themeToggle.addEventListener('click', () => {
+    if (pageTheme.className === 'dark') {
+        pageTheme.className = 'light'
+        header.classList.remove('header-dark')
+        header.classList.add('header-light')
+        themeToggle.src = 'moon.png'
+        cards.forEach(card => {
+            card.classList.remove('greeting-dark')
+            card.classList.add('greeting-light')
+        })
+        likeText.forEach(word => {
+            word.classList.remove('like-dark')
+            word.classList.add('like-light')
+        })
+        likeBtn.forEach(btn => {
+            btn.classList.remove('likeBtn-dark')
+            btn.classList.add('likeBtn-light')
+            btn.src = 'like2.png'
+        })
+        localStorage.setItem('theme', 'light')
+        localStorage.setItem('header', 'header-light')
+        localStorage.setItem('theme-img', 'moon.png')
+        localStorage.setItem('greeting-card', 'greeting-light')
+        localStorage.setItem('like-text', 'like-light')
+        localStorage.setItem('btn', 'likeBtn-light')
+        localStorage.setItem('btn-img', 'like2.png')
+    } else if (pageTheme.className === 'light') {
+        pageTheme.className = 'dark'
+        header.classList.remove('header-light')
+        header.classList.add('header-dark')
+        themeToggle.src = 'sun.png'
+        cards.forEach(card => {
+            card.classList.remove('greeting-light')
+            card.classList.add('greeting-dark')
+        })
+        likeText.forEach(word => {
+            word.classList.remove('like-light')
+            word.classList.add('like-dark')
+        })
+        likeBtn.forEach(btn => {
+            btn.classList.remove('likeBtn-light')
+            btn.classList.add('likeBtn-dark')
+            btn.src = 'like.png'
+        })
+        localStorage.setItem('theme', 'dark')
+        localStorage.setItem('header', 'header-dark')
+        localStorage.setItem('theme-img', 'sun.png')
+        localStorage.setItem('greeting-card', 'greeting-dark')
+        localStorage.setItem('like-text', 'like-dark')
+        localStorage.setItem('btn', 'likeBtn-dark')
+        localStorage.setItem('btn-img', 'like.png')
+    }
+})
+
+// Input fields
+const addGreeting = document.querySelector('.addGreeting')
+const changeGreeting = document.querySelector('.changeGreeting')
+const newGreeting = document.querySelector('.newGreeting')
+
+// Buttons
+const updateBtn = document.querySelector('#updateBtn')
+const delBtn = document.querySelectorAll('.delBtn')
+
+// Greetings list
+const greetings = document.querySelectorAll('.greetingText')
+const greetingBtn = document.querySelectorAll('.greeting')
+
+
+const msg = document.querySelector('.message')
+const likeCount = document.querySelector('.likeCount')
+
+// Send update request for greeting to server on click, reset likes counter
+updateBtn.addEventListener('click', async () => {
+    try {
+        if (!changeGreeting.value) {
+            msg.textContent = 'Choose greeting to update'
+        } else if (!newGreeting.value) {
+            msg.textContent = 'New greeting must not be empty'
+        } else {
+            await fetch('/greetings', {
+                method: 'put',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    greeting: changeGreeting.value,
+                    newGreeting: newGreeting.value,
+                    newGreetingLikes: 0
+                })
+            })
+            window.location.reload(true)
+        }
+    }
+    catch {
+        console.log('Fetch (update greetings) error!')
+    }
+})
+
+// increase like count and update database
+likeBtn.forEach(btn => {
+    btn.addEventListener('click', async () => {
+        try {
+            await fetch('/likes', {
+                method: 'put',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    greeting: btn.classList[1].split('-').join(' ')
+                })
+            })
+            window.location.reload(true)
+        }
+        catch {
+            console.log('Fetch (update likes) error!')
+        }
+    })
+})
+
+// Send delete request to server on button click
+delBtn.forEach(btn => {
+    btn.addEventListener('click', async () => {
+        try {
+            await fetch('/greetings', {
+                method: 'delete',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    greeting: btn.classList[1].split('-').join(' ')
+                })
+            })
+            window.location.reload(true)
+        }
+        catch {
+            console.log('Fetch (delete greeting) error!')
+        }
+    })
+})
+
+// Handle empty list on page load, connect rendered greetings to input field for update/delete requests
+if (!greetings) {
+    msg.textContent = 'Add your first greeting here!'
+} else {
+    for (let greeting of greetings) {
+        greeting.addEventListener('click', () => {
+            changeGreeting.value = greeting.firstElementChild.textContent.trim()
+            // console.log(greeting)
+        })
+    }
+}
---
+++ server.js
@@ -0,0 +1,189 @@
+/* EXPRESS FRAMEWORK */
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+/* MIDDLEWARE */
+
+// Handle cors
+const cors = require('cors')
+app.use(cors())
+
+// Handle JSON
+app.use(express.json())
+
+// Set up views
+app.set('view-engine', 'ejs')
+app.use(express.static('public'))
+
+// Enable form handling
+app.use(express.urlencoded({ extended: true }))
+
+
+
+/* DATABASE | SERVER */
+
+const MongoClient = require('mongodb').MongoClient
+
+// Connect to database on page load
+connectToDb()
+
+async function connectToDb() {
+    try {
+        const client = await MongoClient.connect(process.env.DB_STRING)
+        console.log('Connected to database!')
+
+        const db = client.db(process.env.DB_NAME)
+        const greetingsCollection = db.collection('greetings')
+
+
+        // API - endpoint to access the entire greeting list
+        app.get('/api', async (req, res) => {
+            try {
+                const greetings = await greetingsCollection.find().toArray()
+                const greetingsObj = {}
+
+                for (let i = 0; i < greetings.length; i++) {
+                    let key = greetings[i].greeting.toLowerCase().split(' ').join('-')
+                    greetingsObj[key] = greetings[i]
+                }
+
+                res.json(greetingsObj)
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to retrieve greetings list!'
+            }
+        })
+
+        // API - endpoint to access specific greetings
+        app.get('/api/:greeting', async (req, res) => {
+            try {
+                const greetings = await greetingsCollection.find().toArray()
+                const greetingsObj = {}
+                const empty = "Greeting does not exist!"
+
+                const greeting = req.params.greeting.toLowerCase().split(' ').join('-')
+
+                for (let i = 0; i < greetings.length; i++) {
+                    let key = greetings[i].greeting.toLowerCase().split(' ').join('-')
+                    greetingsObj[key] = greetings[i]
+                }
+
+                if (greetingsObj[greeting]) {
+                    res.json(greetingsObj[greeting])
+                } else {
+                    res.json(empty)
+                }
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to retrieve greeting!'
+            }
+        })
+
+        /* CRUD OPERATIONS */
+
+        // READ - render main page with greetings list
+        app.get('/', async (req, res) => {
+            try {
+                const greetings = await greetingsCollection
+                    .find()
+                    .sort({ likes: -1 })
+                    .toArray()
+                res.render('index.ejs', { greetings: greetings })
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to render greetings!'
+            }
+        })
+
+        // CREATE - add greeting
+        app.post('/greetings', (req, res) => {
+            try {
+                req.body.likes = 0
+                greetingsCollection.insertOne(req.body)
+                console.log(`Added greeting!`)
+                res.redirect('/')
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to add greeting!'
+            }
+        })
+
+        // UPDATE - change greeting
+        app.put('/greetings', (req, res) => {
+            try {
+                greetingsCollection
+                    // findOneAndUpdate() args - key for query filter, update, options
+                    .findOneAndUpdate({ greeting: req.body.greeting }, {
+                        $set: {
+                            greeting: req.body.newGreeting,
+                            likes: req.body.newGreetingLikes
+                        },
+                    }
+                    )
+                res.json().statusCode = 200
+                res.json().statusMessage = 'OK'
+                console.log('Updated greeting!')
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to update greeting!'
+            }
+        })
+
+        // UPDATE increase like count
+        app.put('/likes', async (req, res) => {
+            try {
+                await greetingsCollection
+                    // findOneAndUpdate() args - key for query filter, update, options
+                    .findOneAndUpdate({ greeting: req.body.greeting }, {
+                        $inc: {
+                            likes: 1
+                        },
+                    }
+                    )
+                res.json().statusCode = 200
+                res.json().statusMessage = 'OK'
+                console.log('Updated likes')
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to update likes!'
+            }
+        })
+
+        // DELETE - remove greeting
+        app.delete('/greetings', (req, res) => {
+            try {
+                if (req.body.greeting) {
+                    greetingsCollection
+                        .deleteOne({ greeting: req.body.greeting })
+                    res.json().statusCode = 200
+                    res.json().statusMessage = 'OK'
+                    console.log('Deleted greeting')
+                } else {
+                    console.log('Server error! No greeting to delete!')
+                }
+            }
+            catch {
+                res.json().statusCode = 500
+                res.json().statusMessage = 'Server error! Failed to delete greeting!'
+            }
+        })
+    }
+    catch {
+        console.log('Failed to connect to database!')
+    }
+}
+
+// set port for server
+const PORT = process.env.PORT || 8000
+
+// allow cloud hosting to set port or use local host
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
